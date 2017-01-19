9b9b5c96f21122ab70753307fec50680e840288a
Katarina Ljungdahl <jag@katarinaljungdahl.se>
2017-01-19 11:01:55 +0100

    WIP: added main structure, including folders for sass structure

--- header.php
+++ header.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php wp_title(); ?></title>
-    <?php wp_head(); ?>
-</head>
-<body>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
+		<nav>
+			<?php wp_nav_menu(array('theme_location' => 'mainmenu')); ?>
+		</nav>
