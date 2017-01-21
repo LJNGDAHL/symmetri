3862e8ec9ac9363a9014e06c194b1b339c6e4293
Katarina Ljungdahl <jag@katarinaljungdahl.se>
2017-01-21 15:30:53 +0100

    Added a check to see if user is looking at a gallery page. If yes, loop out gallery images

--- single.php
+++ single.php
@@ -13,15 +13,22 @@ if(have_posts()) {
 		the_post();
 
 		if( has_post_thumbnail() ) {
-			the_post_thumbnail('full');
+			the_post_thumbnail( 'full' );
 		}
 
-		$gallery_images = CFS()->get('gallery_images');
-			foreach ($gallery_images as $image) {
+		// Check if custom page type for the gallery is used.
+		if ( is_singular( 'wpaj_cpt_gallery' ) ) {
+
+			// TODO: put this in a separate file
+			// and include with get_template_part instead.
+			$gallery_images = CFS() -> get ( 'gallery_images' );
+
+			// This loops all the images in the gallery.
+			foreach ( $gallery_images as $image ) {
 				// The class "wp-post-image" is currently styled in _base.scss
 				echo '<img class="wp-post-image" src="'.$image["image"].'"/>';
+			}
 		}
-
 	}
 }
 
