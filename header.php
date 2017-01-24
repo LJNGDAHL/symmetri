9d1c0fcc52ccd2b117001716dd389c11d5112cef
Katarina Ljungdahl <jag@katarinaljungdahl.se>
2017-01-24 11:17:55 +0100

    Added favicons in project and looked at an alternative for menu icon. WIP

--- header.php
+++ header.php
@@ -5,10 +5,20 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 		<title><?php echo get_bloginfo( 'name' ); ?></title>
+		<link rel="apple-touch-icon" sizes="180x180" href="img/favicons/apple-touch-icon.png">
+		<link rel="icon" type="image/png" href="img/favicons/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="img/favicons/favicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="img/favicons/manifest.json">
+		<link rel="mask-icon" href="img/favicons/safari-pinned-tab.svg" color="#15141b">
+		<meta name="theme-color" content="#f8f8f8">
 		<?php wp_head(); ?>
 	</head>
 	<body id="body" <?php body_class(); ?>>
-		<a class="menu-link" href="#navigation">Menu</a>
+		<!-- <a class="menu-link" href="#navigation">Menu</a> -->
+		<!-- TODO: Decide which menu link to use.  -->
+		<a href="#navigation" aria-label="Menu icon">
+			<?php get_template_part( 'img/menu', 'icon' ); ?>
+		</a>
 		<nav class="main-navigation" id="navigation">
 			<a href="#body" class="exit-menu" aria-label="Exit icon">
 				<?php get_template_part( 'img/exit', 'icon' ); ?>
