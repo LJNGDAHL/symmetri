86c12850d46e0c484cb385ac82c4a3ea36e1f9e4
Katarina Ljungdahl <jag@katarinaljungdahl.se>
2017-01-31 09:17:23 +0100

    Fixed bug in isotope_init.js, updated styling in _address.scss, added a new color variable.

--- js/isotope_init.js
+++ js/isotope_init.js
@@ -1,20 +1,32 @@
-// TODO: Figure out how this works and add proper settings if any is needed.
-var elem = document.querySelector('.grid');
+(function () {
+  'use strict';
 
-var iso = new Isotope(elem, {
-  itemSelector: '.grid-item',
+  var iso;
+  var elem = document.querySelector('.grid');
+  var images = document.querySelectorAll('.full-width-img');
 
-  masonry: {
-    gutter: 5
+  if (elem) {
+    /**
+     * Create a grid using Isotope
+     * @type {Isotope}
+     */
+    iso = new Isotope(elem, {
+      itemSelector: '.grid-item',
+      masonry: {
+        gutter: 5
+      }
+    });
+
+    /**
+     * Listen for images to load.
+     */
+    Array.prototype.forEach.call(images, function (img) {
+      img.addEventListener('load', function onLoad() {
+        img.removeEventListener('load', onLoad);
+
+        // Let Isotope recalculate layout.
+        iso.layout();
+      });
+    });
   }
-
-});
-
-var images = document.querySelectorAll('.full-width-img');
-
-Array.prototype.forEach.call(images, function (img) {
-  img.addEventListener('load', function onLoad() {
-    img.removeEventListener('load', onLoad);
-    iso.layout();
-  });
-});
+}());
