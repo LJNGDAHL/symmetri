4c8066cebbb5239ac561d668439d7b69c303bc3a
Katarina Ljungdahl <jag@katarinaljungdahl.se>
2017-02-03 17:15:40 +0100

    Added debouncer and a check if Masonry should be active to prevent resizing issues

--- js/masonry_init.js
+++ js/masonry_init.js
@@ -1,28 +1,64 @@
 (function () {
   'use strict';
 
-  var msnry;
+  var msnry, isActive, timeout;
+  var MEDIAQUERY = '(min-width: 900px), (max-height: 500px) and (orientation: landscape)';
   var elem = document.querySelector('.grid');
   var images = document.querySelectorAll('.full-width-img');
 
   if (elem) {
-    /**
-     * Create a grid using Masonry
-     * @type {Masonry}
-     */
-  msnry = new Masonry(elem, {
-      itemSelector: '.grid-item',
-      gutter: 40
-    });
+
+    msnry = createMsnry();
+
+    toggleMsnry();
 
     /**
      * Listen for images to load.
      */
     Array.prototype.forEach.call(images, function (img) {
-      img.addEventListener('load', function onLoad() {
-        // Let Masonry recalculate layout.
-        msnry.layout();
+      img.addEventListener('load', function () {
+
+        if (isActive) {
+          // Let Masonry recalculate layout.
+          msnry.layout();
+        }
       });
     });
   }
+
+  /**
+   * Debounce toggleMsnry for 200ms on window resize
+   */
+  window.addEventListener('resize', function() {
+    clearTimeout(timeout);
+    timeout = setTimeout(toggleMsnry, 200);
+  });
+
+  /**
+   * Determine whether Msnry should be active or not.
+   */
+  function toggleMsnry() {
+    var shouldBeActive = window.matchMedia(MEDIAQUERY).matches;
+
+    if (shouldBeActive && !isActive) {
+      msnry = createMsnry();
+    } else if (!shouldBeActive && isActive) {
+      msnry.destroy();
+    }
+
+    isActive = shouldBeActive;
+  }
+
+  /**
+   * Create a grid using Masonry
+   * @return {Masonry}
+   */
+  function createMsnry() {
+    return new Masonry(elem, {
+      itemSelector: '.grid-item',
+      gutter: 40,
+      transitionDuration: 0
+    });
+  }
+
 }());
